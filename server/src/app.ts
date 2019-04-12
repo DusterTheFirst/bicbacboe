809f6d04e5b053ca58e3eee1bb1bcfc924ce0c5e
DusterTheFirst <dusterthefirst@users.noreply.github.com>
2019-04-12 15:28:13 -0400

    Layout for typing packets

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,9 @@
-import { ClientErrorCode } from "../../protocol/src/ws";
+import { version } from "@bicbacboe/protocol/build/index";
 import express from "express";
 
 const app = express();
 
-const port = process.env.PORT === null ? 8080 : process.env.PORT;
+const port = process.env.PORT == null ? 8080 : process.env.PORT;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-console.log(ClientErrorCode);
+console.log(version);
