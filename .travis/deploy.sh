a8df32cd9905200addcd9aac01aef27733ea49ff
Zachary Kohnen <14093962+DusterTheFirst@users.noreply.github.com>
2019-04-26 21:12:12 -0400

    Use travis Script deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,20 +5,19 @@ function deployDirectory() {
     DIRECTORY=$2
 
     rsync -rq --delete -e 'ssh -p 4220' --rsync-path="rm -rf bicbacboe/$CHANNEL/$DIRECTORY && mkdir -p bicbacboe/$CHANNEL/$DIRECTORY && rsync" $TRAVIS_BUILD_DIR/$DIRECTORY/build/ travis@ssh.bicbacboe.com:bicbacboe/$CHANNEL/$DIRECTORY
-    ssh -p 4220 pi@ssh.bicbacboe.com "tree -h bicbacboe/$CHANNEL/$DIRECTORY"
+    ssh -p 4220 travis@ssh.bicbacboe.com "tree -h bicbacboe/$CHANNEL/$DIRECTORY"
 }
 
-if [ $# -eq 0 ]; then
-    echo "You must supply a folder to deploy"
+if [ $# = 0 ] || ([ $1 != "staging" ] && [ $1 != "stable" ]); then
+    echo "You must supply a valid branch to deploy to"
     exit 1
 fi
 
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    echo "Deploying $1 to the STABLE branch"
-    deployDirectory stable $1
-else
-    echo "Deploying $1 to the STAGING branch"
-    deployDirectory staging $1
-fi
+echo "Building"
+yarn run build
+echo "Completed build"
+
+deployDirectory $1 client
+deployDirectory $1 server
 
 echo "Deployed"
